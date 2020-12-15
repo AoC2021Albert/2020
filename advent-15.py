863035d111c1ca4a33e363bcc961dd2f01dfdbba
Albert Bendicho <albert.bendicho@capside.com>
2020-12-15 08:55:21 +0100

    Add day 15

---
+++ advent-15.py
@@ -0,0 +1,44 @@
+def a(m,limit):
+    turn=len(m)
+    last_spoken = m[-1]
+
+    while turn < 2020:
+        turn += 1
+        try:
+            distance = list(reversed(m[:-1])).index(last_spoken)+1
+        except ValueError:
+            distance = 0
+        m.append(distance)
+        last_spoken = distance
+    return(m[-1])
+
+def ab(m, limit):
+    d = dict()
+    for i, v in enumerate(m[:-1]):
+        d[v] = i + 1
+    turn=len(m)
+    last_spoken = m[-1]
+
+    while turn < limit:
+        if last_spoken in d:
+            distance = turn - d[last_spoken]
+        else:
+            distance = 0
+        d[last_spoken] = turn
+        turn += 1
+        last_spoken = distance
+    return(distance)
+
+
+
+for limit in [2020, 30000000]:
+    print(ab([9,3,1,0,8,4],limit))
+    print(ab([0,3,6],limit))
+    print(ab([1,3,2],limit))
+    print(ab([2,1,3],limit))
+    print(ab([1,2,3],limit))
+    print(ab([2,3,1],limit))
+    print(ab([3,1,2],limit))
+
+
+
